9f1ea9fcb3b4d7b3023b60ff0d0174801512035f
TuPu <141525093+TuuPuu@users.noreply.github.com>
2023-11-28 20:45:04 +0000

    fixed git status as well as created a for loop to generate a random password based on user input

---
+++ starter/script.js
@@ -0,0 +1,126 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+// CREATING CODE THAT CREATES A RANDOM SELECTIN FROM SPECIAL CHARACTER ARRAY BASED ON NUMBER CHOSEN
+
+let numberChosen = prompt(
+  "How many special characters would you like? Please pick between 2 and 32"
+);
+
+for (let i = 0; i < numberChosen; i++) {
+  console.log(
+    specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+  );
+}
+
+// const randomSpecialCharacter = 0;
+
+// Function to prompt user for password options
+function getPasswordOptions() {}
+
+// Function for getting a random element from an array
+function getRandom(arr) {}
+
+// Function to generate password with user input
+function generatePassword() {}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
